cd6fdf26ded3155164a653d971d5544afd3f6d09
Kamal16987 <kannan.kamal87@gmail.com>
2019-03-29 09:10:03 +0530

    Created a baseclass with launch browser method

---
+++ src/com/test/packagae/BasaeClass.java
@@ -0,0 +1,14 @@
+package com.test.packagae;
+
+public class BasaeClass {
+
+	public static void main(String[] args) {
+		System.out.println("Created a Baseclass....");
+
+	}
+	
+	public void launchBrowser() {
+		System.out.println("Method for Launching browser");
+	}
+
+}
